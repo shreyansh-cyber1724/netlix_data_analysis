c5398cd5a46d3d9c577eff7b0b6242fc19eadc2b
Shreyansh Attri Rishi <shreattri@gmail.com>
2026-01-15 21:39:12 +0530

    added more analysis and ploted the graph

--- Netflix_analysis.py
+++ Netflix_analysis.py
@@ -1,19 +1,18 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 #for laoding data of netflix
 df = pd.read_csv("netflix_titles.csv")
+
+
 #for getting netflix data rows and coloumn
-print(df.shape)
-#for getting netflix's coloumn data type and info
+
 
 print(df.info()) #for data type and non null for missing value
 
 print(df.isnull().sum()) # for getting count of null value in coloumn
-#now we got to find percentage missing value with this 
-print(df.isnull().sum() / df.shape[0] * 100)
-
-mising = df.isnull().sum()
-mising = mising[mising > 0] #with boolean masking to get only missing data count
-print(mising)
+# #now to get percentage missing value with this 
+# print(df.isnull().sum() / df.shape[0] * 100)
 
 #filling missing data
 df["director"] = df["director"].fillna("Unknown") 
@@ -21,14 +20,59 @@ df["cast"] = df["cast"].fillna("Unknown")
 df["country"] = df["country"].fillna("Unknown")
 df["rating"] = df["rating"].fillna("Unknown")
 
-#droping row with 
+#droping row with missing data
 df = df.dropna(subset =["date_added"])
-df = df.dropna(subset=["duration"])
 
-#groupby
+# #reset indexing
+df = df.reset_index(drop=True)
+
+#for date time analysis
+
+df["date_added"]=df["date_added"].str.strip() #for treating whitespace after or before
+df["date_added"]= pd.to_datetime(df["date_added"]) #changing data to date time data type
 
 
+print(df.info())
+
+df["year_added"] = df['date_added'].dt.year #to fetch year in date_added coloumn
+
+g6 = df.groupby("year_added").size().sort_index()
+print(g6)
+
+#from the g6 we get to know that 2019 have highest number of movie and series
+#and growth till 2019 but then decrease 2020
+
+g1 = df.groupby(["year_added", "type"]).size().unstack()
+print(g1) #this shows more detieled yearly data
+
+g2 = df["year_added"].value_counts().idxmax()
+print(g2)
 
 
-#for getting netflix data's important inspection
-#print(df.describe())
+# ploting graph 
+
+
+table = df.groupby(["year_added", "type"])["show_id"].count().unstack(fill_value=0).sort_index()
+table.plot(figsize=(10,6), marker='o', grid=True)
+plt.title("Netflix Content Added per Year")
+plt.xlabel("Year Added")
+plt.ylabel("Number of Titles")
+
+
+#plt.show()#for ploting the graph
+
+#for bar chart ploting
+type_cont = df["type"].value_counts()
+plt.figure()
+plt.bar(type_cont.index,type_cont.values)
+plt.xlabel("type")
+plt.ylabel("count")
+plt.title("movies vs tv shows on netflix")
+plt.show()
+
+
+Summury = df.groupby("type").agg({
+    "release_year":["min","max","mean"],
+    "show_id" :"count"
+})
+print(Summury)
